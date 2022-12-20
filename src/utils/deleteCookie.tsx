1737dcf97bcc96ec9a13cdc7eb39cf5078587703
Leonardo.Arinabar <leonardo.arinabar@spreetail.com>
2022-12-19 20:10:20 -0600

    style:Formating Code

--- src/utils/deleteCookie.tsx
+++ src/utils/deleteCookie.tsx
@@ -1,26 +1,25 @@
-export const deleteCookie=(analytics:boolean)=>{
-    if(analytics===true){
-        let cookies = document.cookie.split(";");
-        if(cookies.length){
-            cookies=cookies.map((cook)=>cook.trim())
-        }
-        for (var i = 0; i < cookies.length; i++) {
-            const cookie = cookies[i];
-            if(typeof cookie ==='string'){
-                    deleteAnalyticsCookies(cookie)
-            }    
-        }
+export const deleteCookie = (analytics: boolean) => {
+  if (analytics === true) {
+    let cookies = document.cookie.split(";");
+    if (cookies.length) {
+      cookies = cookies.map((cook) => cook.trim());
     }
-    
-}
-   
-const deleteAnalyticsCookies=(cookie:string)=>{
-    if(cookie.search(/^_ga_\w+/)===0||cookie.search(/^_ga_/)===0){
-        document.cookie=cookie+";max-age=0"; 
-        console.log('deleteAnalyticsCookies.cookie',cookie)
-        }
-        if(cookie.search(/^_ga=\w+/)===0){
-            document.cookie=cookie+";max-age=0";
-            console.log('deleteAnalyticsCookies.cookie',cookie) 
-        }
-}
+    for (var i = 0; i < cookies.length; i++) {
+      const cookie = cookies[i];
+      if (typeof cookie === "string") {
+        deleteAnalyticsCookies(cookie);
+      }
+    }
+  }
+};
+
+const deleteAnalyticsCookies = (cookie: string) => {
+  if (cookie.search(/^_ga_\w+/) === 0 || cookie.search(/^_ga_/) === 0) {
+    document.cookie = cookie + ";max-age=0";
+    console.log("deleteAnalyticsCookies.cookie", cookie);
+  }
+  if (cookie.search(/^_ga=\w+/) === 0) {
+    document.cookie = cookie + ";max-age=0";
+    console.log("deleteAnalyticsCookies.cookie", cookie);
+  }
+};
